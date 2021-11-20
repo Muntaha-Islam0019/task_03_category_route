1ac53a7f2b3071fd3c0ed5d19bc9f160fee8abe0
Muntaha-Islam0019 <muntaha.islam0019@gmail.com>
2021-11-20 16:16:28 +0600

    Edited category_routes.dart

--- lib/category_route.dart
+++ lib/category_route.dart
@@ -8,7 +8,11 @@ import 'package:flutter/material.dart';
 
 // TODO: Check if we need to import anything
 
+import 'package:task_03_category_route/category.dart';
+
 // TODO: Define any constants
+
+final _backgroundColor = Colors.green[100];
 
 /// Category Route (screen).
 ///
@@ -42,17 +46,48 @@ class CategoryRoute extends StatelessWidget {
     Colors.red,
   ];
 
+  // For portrait, we construct a [ListView] from the list of category widgets.
+  Widget _buildCategoryWidgets(List<Widget> categories) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) => categories[index],
+      itemCount: categories.length,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: Create a list of the eight Categories, using the names and colors
     // from above. Use a placeholder icon, such as `Icons.cake` for each
     // Category. We'll add custom icons later.
+    final categories = <Category>[];
+    for (var i = 0; i < _categoryNames.length; i++) {
+      categories.add(Category(
+        name: _categoryNames[i],
+        color: _baseColors[i] as ColorSwatch<dynamic>,
+        iconLocation: Icons.cake,
+      ));
+    }
 
     // TODO: Create a list view of the Categories
-    final listView = Container();
+    final listView = Container(
+      color: _backgroundColor,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: _buildCategoryWidgets(categories),
+    );
 
     // TODO: Create an App Bar
-    final appBar = AppBar();
+    final appBar = AppBar(
+      elevation: 0.0,
+      title: const Text(
+        'Unit Converter',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 30.0,
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: _backgroundColor,
+    );
 
     return Scaffold(
       appBar: appBar,
